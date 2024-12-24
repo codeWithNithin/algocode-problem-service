3beefd23ee3b5cf14d0c87efde524bb4a5f23c5b
nithin kumar <nithinkottary25@gmail.com>
2024-12-24 21:14:11 +0530

    Fix: added not implemented status code for all unimplemented routes

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -9,19 +9,19 @@ return res.status(StatusCodes.NOT_IMPLEMENTED).json({message: 'Not Implemented !
 }
 
 function getProblem(req, res) {
-  return res.status(501).json({message: 'Not Implemented !!!'})
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({message: 'Not Implemented !!!'})
 }
 
 function getProblems(req, res) {
-  return res.status(501).json({message: 'Not Implemented !!!'})
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({message: 'Not Implemented !!!'})
 }
 
 function deleteProblem(req, res) {
-  return res.status(501).json({message: 'Not Implemented !!!'})
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({message: 'Not Implemented !!!'})
 }
 
 function updateProblem(req, res) {
-  return res.status(501).json({message: 'Not Implemented !!!'})
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({message: 'Not Implemented !!!'})
 }
 
 module.exports = {
